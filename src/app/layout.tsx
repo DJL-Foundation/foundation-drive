7b58f6149d0bf889da06408747eb24b4a953a638
DJL - Jack <71508487+JackatDJL@users.noreply.github.com>
2025-02-12 23:53:41 +0100

    Added Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -14,6 +16,9 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
+      <Head>
+        <Analytics />
+      </Head>
       <body>{children}</body>
     </html>
   );
